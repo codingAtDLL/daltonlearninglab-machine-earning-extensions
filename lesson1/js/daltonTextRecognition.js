73a6df2a738a1e004d855ebe8a635be321bda469
codingAtDLL <40426756+codingAtDLL@users.noreply.github.com>
2018-06-21 09:51:17 +0800

    lesson1
    Lesson 1 is an example project about home automation, which can be accessed at https://ai4children.org/lesson1
    
    p.s. further few clean up of files may be needed

---
+++ lesson1/js/daltonTextRecognition.js
@@ -0,0 +1,121 @@
+(function(ext) {
+
+
+
+    window.newTextRecognition = function() {
+        window.TextClassifier = limdu.classifiers.multilabel.BinaryRelevance.bind(0, {
+            binaryClassifierType: limdu.classifiers.SvmJs.bind(0, {C: 1.0})
+        });
+        window.intentClassifier = new limdu.classifiers.EnhancedClassifier({
+            classifierType: TextClassifier,
+            normalizer: limdu.features.LowerCaseNormalizer,
+            featureExtractor: limdu.features.NGramsOfWords(1),  // each word ("1-gram") is a feature  
+            featureLookupTable: new limdu.features.FeatureLookupTable()
+        });
+        window.trainingBatch = [];
+    }
+    window.newTextRecognition();
+    
+
+    ext._shutdown = function() {
+        
+    };
+
+    ext._getStatus = function() {
+        if (window.webkitSpeechRecognition === undefined) {
+            return {status: 1, msg: 'Your browser does not support speech recognition. Try using Google Chrome.'};
+        }
+        else if (window.SpeechSynthesisUtterance === undefined) {
+            return {status: 1, msg: 'Your browser does not support text to speech. Try using Google Chrome.'};
+        }
+        return {status: 2, msg: 'Ready'};
+    };
+
+    /************\
+    |   BLOCKS   |
+    \************/
+
+    window.recognized_speech = '';
+
+    ext.newTextRecognition = function () {
+         window.newTextRecognition();
+    };
+
+    ext.addTrainingRecord = function (pInput, pLabel) {
+         window.trainingBatch.push({input:pInput, output:pLabel});
+    };
+
+    ext.train = function (callback) {
+         window.intentClassifier.trainBatch(window.trainingBatch);
+         callback();
+    };
+
+    ext.classifyText = function (pInput, callback) {
+         var result = window.intentClassifier.classify(pInput);
+         if (result.length) {
+            callback(result[0]);
+         }
+         else {
+            callback("");
+         }         
+    };
+
+    ext.classifyTextAsCSV = function (pInput, callback) {
+         callback(window.intentClassifier.trainBatch(window.trainingBatch));
+    };
+
+    ext.speak_text = function (text, callback) {
+        var u = new SpeechSynthesisUtterance(text.toString());
+        u.onend = function(event) {
+            if (typeof callback=="function") callback();
+        };        
+        speechSynthesis.speak(u);
+    };
+
+    ext.recognize_speech = function (callback) {
+        window.recognized_speech = "";
+        window.recognition = new webkitSpeechRecognition();
+        window.recognition.onresult = function(event) {
+            console.log("result");
+            window.recognized_speech = "";
+            if (event.results.length > 0) {
+                window.recognized_speech = event.results[0][0].transcript;
+                console.log("result length > 0 " + window.recognized_speech);
+            }
+            //window.recognition.stop();
+            //callback();
+
+        };
+        window.recognition.onnomatch = function(event) {
+            console.log("no match / on error");
+            callback();
+        };
+        window.recognition.onaudioend = function(event) {
+            console.log("onaudioend");
+            setTimeout(callback, 200);
+        };
+        window.recognition.start();
+    };
+
+    ext.recognized_speech = function () {return recognized_speech;};
+
+    var descriptor = {
+        blocks: [
+            [' ', 'new Text Recognition', 'newTextRecognition'],
+            [' ', 'reset Text Recognition', 'newTextRecognition'],
+            [' ', 'add training record %s with label %s', 'addTrainingRecord', 'text', 'label'],
+            ['w', 'train', 'train'],
+            ['R', 'classify text: %s', 'classifyText', 'text'],
+            ['R', 'classify text: %s and return csv', 'classifyTextAsCSV', 'text'],
+            ['w', 'speak %s', 'speak_text', 'Hello!'],
+            ['w', 'wait and recognize speech', 'recognize_speech'],
+            ['r', 'recognized speech', 'recognized_speech']
+        ]
+    };
+
+    ScratchExtensions.register('Dalton-Text-Recognition', descriptor, ext);
+    
+    
+
+
+})({});
